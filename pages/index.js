3eeca7d5107faa889760ef7191edd463c5cd3ca8
Daz Devoloper Viyolen <pribadiarep2@gmail.com>
2025-05-27 17:24:12 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
     <>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <title>AI MTK by Alecia</title>
+        <title>AI MTK by Arewp</title>
       </head>
       <main style={{
         padding: 20,
